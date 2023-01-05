c0fc93935d54c4f10f3d201f3c600fd879ea7c2c
Donna Kuang <donna.kuang@alarispro.com>
2023-01-04 21:02:32 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises");
+const express = require("express");
+const app = express();
+
+
+app.get("/", (request, response) => {
+    
+    //-- promise-based
+    async function sendHome() {
+        try {
+            const home = await fs.readFile("./index.html", { encoding: "utf8" });
+            response.send(home);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    sendHome();
+
+});
+
+// listen to incoming 
+app.listen(process.env.PORT || 3000, () => console.log("App available on http://localhost:3000"))
